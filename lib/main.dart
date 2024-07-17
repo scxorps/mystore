262ba80c1a6196b4a7d12e4920b5f055d09bb213
scxorps <mohamed79163@gmail.com>
2024-07-17 23:05:37 +0100

    updated visuals

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:medstore/screens/homescreen.dart';
 import 'constant.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
+
 
 void main() {
   runApp(MyApp());
